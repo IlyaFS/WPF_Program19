bf3ef2f770fc38f4a8fae56e9cd1ac1608f675b5
Илья Филиппов <filippov.i.s@gmail.com>
2022-03-07 17:52:05 +0300

    Добавьте файлы проекта.

---
+++ WPF_Program19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_Program19.Models;
+
+namespace WPF_Program19.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string Propertyname=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Propertyname));
+        }
+        private int radius;
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double arcLenght;
+        public double ArcLenght
+        {
+            get => arcLenght;
+            set
+            {
+                arcLenght = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            ArcLenght = ArcMath.Add(Radius);
+        }
+        private bool CanAddCommandExecute(object p)
+        {
+            if (radius != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+    }
+}
